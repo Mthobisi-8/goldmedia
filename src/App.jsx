7ca15c4f0cebf314c6a8fe98f6b7c065b6015e52
Mthobisi Motimele <motimelemthobisi@gmail.com>
2025-08-20 15:24:40 +0200

    lets see

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,29 @@
-import About from "./Components/About"
-import HomePage from "./Components/HomePage"
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route
-} from "react-router-dom";
-import Service from "./Components/Service";
-import Portfolio from "./Components/Portfolio";
-import ContactUs from "./Components/ContactUs";
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from 'react-router-dom';
+import HomePage from './Components/HomePage';
+import About from './Components/About';
+import Service from './Components/Service';
+import Portfolio from './Components/Portfolio';
+import ContactUs from './Components/ContactUs';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route index='/HomePage' element={<HomePage />} />
-      <Route path='/HomePage' element={<HomePage />} />
-      <Route path='/About' element={<About />} />
-      <Route path='/Service' element={<Service />} />
-      <Route path='/Portfolio' element={<Portfolio />} />
-      <Route path='/ContactUs' element={<ContactUs />} />
-      
-
+      <Route path="/" element={<HomePage />} />
+      <Route path="/HomePage" element={<HomePage />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Service" element={<Service />} />
+      <Route path="/Portfolio" element={<Portfolio />} />
+      <Route path="/ContactUs" element={<ContactUs />} />
     </>
   )
-)
-
-;
+);
 
 function App() {
-
   return (
-    <div >
-     
+    <div>
       <RouterProvider router={router} />
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
